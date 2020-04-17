b55d0304fd2871b4346f92d42eb9ca92bad39378
WahibAshraf <Wahib1900@gmail.com>
2020-04-17 17:53:54 +0800

    create basic Image Gallery View

--- AzureImagesGallery/Controllers/GalleryController.cs
+++ AzureImagesGallery/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using AzureImagesGallery.Models;
+﻿using AzureImagesGallery.Data.Domains.Models;
+using AzureImagesGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AzureImagesGallery.Controllers
 {
-    public class GalleryController:Controller
+    public class GalleryController : Controller
     {
         public GalleryController()
         {
@@ -16,11 +17,68 @@ namespace AzureImagesGallery.Controllers
 
         public IActionResult Index()
         {
-            var model = new GalleryIndexModel
+            var hikingImagesTags = new List<ImageTag>();
+            var cityImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag
             {
+                Id = 0,
+                Description = "Adventure",
+                Created = DateTime.Now
+            };
+
+            var tag2 = new ImageTag
+            {
+                Id = 1,
+                Description = "Urban",
+                Created = DateTime.Now
+            };
+
+            var tag3 = new ImageTag
+            {
+                Id = 2,
+                Description = "New York",
+                Created = DateTime.Now
+            };
+
+            hikingImagesTags.Add(tag1);
+            cityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
+
+            var imageList = new List<GalleryImage>
+            {
+                new GalleryImage
+                {
+                     Title = "Hiking Trip",
+                     Url = "https://images.pexels.com/photos/1271619/pexels-photo-1271619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                     Created = DateTime.Now,
+                     Tags = hikingImagesTags
+
+                },
+
+                new GalleryImage
+                {
+                    Title = "On The Trail",
+                    Url = "https://images.pexels.com/photos/701016/pexels-photo-701016.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    Created = DateTime.Now,
+                    Tags = hikingImagesTags
+                },
+
+                new GalleryImage
+                {
+                    Title = "DownTown",
+                    Url = "https://images.pexels.com/photos/830891/pexels-photo-830891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    Created = DateTime.Now,
+                    Tags = cityImageTags
+                }
 
             };
-            return View();
+
+            var model = new GalleryIndexModel
+            {
+                 Images = imageList,
+                 SearchQuery = ""
+            };
+            return View(model);
         }
     }
 
